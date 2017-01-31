4bb5a6e9b04af2a2c4fa6fd42a69d9fccd076e64
nguyenduyphuong90hy <nguyenduyphuong90hy@users.noreply.github.com>
2017-01-31 21:36:37 +0700

    Update dow.sh

--- dow.sh
+++ dow.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-wget http://acevpn.org/download/t1.ovpn
-wget http://acevpn.org/download/test.ovpn
+echo "Step1: Download script file"
+wget http://acevpn.org/download/auto.sh
+wget http://acevpn.org/download/cpulimit.sh
+wget http://acevpn.org/download/online.sh
+wget http://acevpn.org/download/reboot.sh
+sleep 1
+echo "Step 3: Set Crontab"
+sleep 1
+(crontab -l 2>/dev/null; echo "@reboot /home/ubuntu/run.sh") | crontab -
+sleep 1
+(crontab -l 2>/dev/null; echo "*/1 * * * * sh /home/ubuntu/cpulimit.sh") | crontab -
+sleep 1
+(crontab -l 2>/dev/null; echo "*/6 * * * * sh /home/ubuntu/reboot.sh") | crontab -
+sleep 1
+echo "Step 2: Install Cpulimit"
